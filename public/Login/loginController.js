074092475a91eeb6b8202451de57dbfa757b78da
Sonia Mannan <soniamannan@pcp129782pcs.wireless.calpoly.edu>
2017-05-03 10:05:02 -0700

    inital commit

---
+++ public/Login/loginController.js
@@ -0,0 +1,17 @@
+app.controller('loginController',
+ ['$scope', '$state', 'login', 'notifyDlg',
+ function($scope, $state, login, nDlg) {
+
+// Autologin for testing: $scope.user = {email: "cstaley@calpoly.edu", password: "x"};
+
+   $scope.login = function() {
+      login.login($scope.user)
+      .then(function(user) {
+         $scope.$parent.user = user;
+         $state.go('home');
+      })
+      .catch(function() {
+         nDlg.show($scope, "That name/password is not in our records", "Error");
+      });
+   };
+}]);
